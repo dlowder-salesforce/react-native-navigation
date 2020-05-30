923a817e50c1477b35afa6493bc129a7154ddef0
Douglas Lowder <dlowder@salesforce.com>
2020-05-29 19:27:33 -0700

    Add lib/dist so we can use this branch in tests

---
+++ lib/dist/index.d.ts
@@ -0,0 +1,11 @@
+import { NavigationRoot } from './Navigation';
+export declare const Navigation: NavigationRoot;
+export * from './adapters/Constants';
+export * from './interfaces/ComponentEvents';
+export * from './interfaces/Events';
+export * from './interfaces/EventSubscription';
+export * from './interfaces/Layout';
+export * from './interfaces/Options';
+export * from './interfaces/NavigationComponent';
+export * from './interfaces/NavigationComponentProps';
+export * from './interfaces/NavigationComponentListener';
